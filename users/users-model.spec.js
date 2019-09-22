0f873da5cb8b1f15c6910550b6e048aa271d54a0
Scott Renz <scottrenz@scottrenz.com>
2019-09-22 19:49:07 -0400

    made tests

--- users/users-model.spec.js
+++ users/users-model.spec.js
@@ -4,7 +4,7 @@ const db = require('../database/dbConfig.js');
 
 describe('users model', () => {
   beforeEach(async () => {
-    await db('users').truncate();
+    await db('campaign').truncate();
   });
 
   it('should set environment to testing', () => {
@@ -14,38 +14,26 @@ describe('users model', () => {
   describe('add()', () => {
     it('should add users into the db', async () => {
       // add a record
-      await Users.add({ username: 'Gaffer', password: 'pass' });
-      await Users.add({ username: 'Frodo', password: 'pass' });
-
+      await Users.add('campaign',{ "name":"Save the Whales","description":"Stop people from killing whales","goal":1000000 });
+      await Users.add('campaign',{ "name":"Feed the Children","description":"Help starving children","goal":50000 });
+      await Users.add('campaign',{ "name":"Stop Insomnia","description":"Help people sleep","goal":60000 });
+      // [{"id":1,"name":"Save the Whales","description":"Stop people from killing whales","goal":1000000},{"id":2,"name":"Feed the Children","description":"Help starving children","goal":50000},{"id":3,"name":"Stop Insomnia","description":"Help people sleep","goal":60000}]
       let users = await db('users');
 
       // assert the record was added
-      expect(users).toHaveLength(2);
+      expect(users).toHaveLength(3);
     });
 
-    it('should add users into the db', async () => {
-      // add a record
-      const userid = await Users.add({ username: 'Gaffer', password: 'pass' });
-// console.log('user id',userid.id)
-const id = userid.id
-
-      let user = await db('users')
-        .where({ id })
-        .first();
-
-      // assert the record was added
-      expect(user.username).toBe('Gaffer');
-    });
   });
 
   describe('axios get', () => {
-    it('should get jokes from the site', async () => {
+    it('should get routes from the site', async () => {
         const requestOptions = {
           headers: { accept: 'application/json' },
         };
       
-        let jokes = await  axios.get('https://icanhazdadjoke.com/search', requestOptions)
-        expect(jokes.data.results.length).toBe(20);
+        let jokes = await  axios.get('https://donation-management.herokuapp.com/donate', requestOptions)
+        expect(jokes.data.results.length).toBe(30);
       });
       it('should get jokes from the site', async () => {
         const requestOptions = {
@@ -53,43 +41,43 @@ const id = userid.id
         };
       
         let jokes = await  axios.get('https://icanhazdadjoke.com/search', requestOptions)
-        expect(jokes.data.results).toHaveLength(20);
+        expect(jokes.data.results).toHaveLength(30);
       });
     
   });
   
   describe('find()', () => {
     it('should find users from the db', async () => {
-        let users = await Users.find();
+        let users = await Users.find('member');
         
-        expect(users).toHaveLength(0);
+        expect(users).toHaveLength(7);
       });
   
       it('should find users from the db', async () => {
-        const userid = await Users.add({ username: 'Gaffer', password: 'pass' });
+        const userid = await Users.add('member',{ username: 'Gaffer', password: 'pass', type: 'user' });
 
-        let users = await Users.find();
+        let users = await Users.find('member');
         
-        expect(users).toHaveLength(1);
+        expect(users).toHaveLength(8);
       });
   
       });
   
   describe('findBy()', () => {
     it('should find by users from the db', async () => {
-        const userid = await Users.add({ username: 'Gaffer', password: 'pass' });
+//        const userid = await Users.add({ username: 'Gaffer', password: 'pass' });
   // console.log('user id',userid.id)
-  const id = userid.id
+  const id = 1
         let users = await Users.findBy({id: id});
         
         expect(users).toHaveLength(1);
       });
   
       it('should find by users from the db', async () => {
-        const userid = await Users.add({ username: 'Gaffer', password: 'pass' });
+//        const userid = await Users.add({ username: 'Gaffer', password: 'pass' });
   // console.log('user id',userid.id)
-  const username = userid.username
-        let users = await Users.findBy({username: username});
+  // const username = userid.username
+        let users = await Users.findBy({username: 'Gaffer'});
         
         expect(users).toHaveLength(1);
       });
@@ -98,10 +86,10 @@ const id = userid.id
 
   describe('findById(id)', () => {
     it('should find by id users from the db', async () => {
-      const userid = await Users.add({ username: 'Gaffer', password: 'pass' });
+ //     const userid = await Users.add({ username: 'Gaffer', password: 'pass' });
 // console.log('user id',userid.id)
 const id = userid.id
-      let users = await Users.findById(id);
+      let users = await Users.findById(1);
    //   console.log('findbyid users',users)
       expect(users.id).toBe(1);
     });
@@ -112,33 +100,25 @@ const id = userid.id
     it('should delete a user from the db', async () => {
         // delete a record
         // await Users.add({ username: 'Gaffer' });
-        const userid = await Users.add({ username: 'Gaffer', password: 'pass' });
+        const userid = await Users.add({ username: 'Frodo', password: 'pass', type: 'board' });
         const id = userid.id
         await Users.remove(id);
-        const userid2 = await Users.add({ username: 'Frodo', password: 'pass' });
-        const id2 = userid2.id
-        await Users.remove(id2);
   
-        let users = await db('users');
+        let users = await db('member');
   
         // assert the record was added
-        expect(users).toHaveLength(0);
+        expect(users).toHaveLength(9);
       });
   
       it('should delete a user from the db', async () => {
         // delete a record
         // await Users.add({ username: 'Gaffer' });
-        const userid = await Users.add({ username: 'Joe', password: 'pass' });
-        const id = userid.id
-        await Users.remove(id);
-        const userid2 = await Users.add({ username: 'Moe', password: 'pass' });
-        const id2 = userid2.id
-        await Users.remove(id2);
+        await Users.remove(8);
   
-        let users = await db('users');
+        let users = await db('member');
   
         // assert the record was added
-        expect(users).toHaveLength(0);
+        expect(users).toHaveLength(7);
       });
   
       });
